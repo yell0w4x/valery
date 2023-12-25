91232c92bf2c20d0b0fcb74cdeef0a69cb0ed270
yell0w4x <yellow4x@gmail.com>
2023-12-25 23:42:22 +0200

    Message flow fixes; Chat is working now

--- src/ai.py
+++ src/ai.py
@@ -1,4 +1,8 @@
 import openai
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 class Assistant:
@@ -11,24 +15,27 @@ class Assistant:
                               request_timeout=60.0)
 
     def __init__(self, client, config, model='meta-llama/Llama-2-70b-chat-hf'):
+        _logger.debug(config)
         self.__client = client
         self.__config = config
         self.__model = model
 
 
     async def send_message(self, message, message_history, chat_mode):
+        client = self.__client
         reply = await client.chat.completions.create(
             model = 'meta-llama/Llama-2-70b-chat-hf', 
             messages = self.__adapt_message_history(message, message_history, chat_mode),
             stream = False
         )
-        response = reply['choices']['message']['content']
-        usage_stat = reply['usage']
+        _logger.debug(reply)
+        response = reply.choices[0].message.content
+        usage_stat = reply.usage
         return response, usage_stat
 
 
     def __adapt_message_history(self, message, message_history, chat_mode):
-        prompt = self.__config.chat_modes[chat_mode]["prompt_start"]
+        prompt = self.__config['chat_modes'][chat_mode]['prompt_start']
         return ([dict(role='system', content=prompt)] + 
                 [dict(role=item.role, content=item.content) for item in message_history] +
                 [dict(role='user', content=message)])
